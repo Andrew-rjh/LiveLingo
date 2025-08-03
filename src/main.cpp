4e07f6304b0ee5dc773d69a27c7bfb5a7f1a6c9f
Jinho Ryu <dbwlsgh57@naver.com>
2025-08-03 16:02:52 +0900

    chore: change steps

--- src/main.cpp
+++ src/main.cpp
@@ -41,9 +41,9 @@
 // command-line parameters
 struct whisper_params {
     int32_t n_threads  = std::min(4, (int32_t) std::thread::hardware_concurrency());
-    int32_t step_ms    = 3000;
+    int32_t step_ms    = 500;
     int32_t length_ms  = 10000;
-    int32_t keep_ms    = 200;
+    int32_t keep_ms    = 100;
     int32_t capture_id = -1;
     int32_t max_tokens = 32;
     int32_t audio_ctx  = 0;
